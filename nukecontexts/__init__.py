7613c8386f23b51102ea38e00ab9cc88900dbb66
Florian Einfalt <florian.einfalt@me.com>
2017-02-09 15:19:50 +0000

    Make runtime check respect Windows and macOS platforms

--- nukecontexts/__init__.py
+++ nukecontexts/__init__.py
@@ -1,8 +1,19 @@
 import re
 import sys
+import platform
 
-match = re.search(r'Nuke\d+\.\d+.exe', sys.executable)
+if platform.system() == 'Darwin':
+    application = r'Nuke\d+\.\d+v\d+.app'
+elif platform.system() == 'Windows':
+    application = r'Nuke\d+\.\d+.exe'
+else:
+    raise RuntimeError('OS {0} is not supported'.format(platform.system()))
+
+match = re.search(application, sys.executable)
 if not match:
     raise RuntimeError('Import nukecontexts from within Nuke')
 
 __version__ = '0.1'
+__all__ = ['ctx']
+
+from ctx import enabled, disabled, set_attr_to, multiple_contexts
