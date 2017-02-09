879a73f9e76d0d1c4ef88d64838d94b2c082de8a
Florian Einfalt <florian.einfalt@me.com>
2017-02-09 19:00:20 +0000

    Update the license

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name = 'nukecontexts',
     version = '0.1',
     description='Library of composable context managers for Nuke',
-    url='http://www.florianeinfalt.de',
+    url='https://github.com/florianeinfalt/nukecontexts',
     author='Florian Einfalt',
     author_email='info@florianeinfalt.de',
-    license='MIT',
+    license='BSD',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False
