1a34bdc424065e598d7b63c283c83500a14f58a6
Florian Einfalt <florian.einfalt@me.com>
2017-02-12 18:36:42 +0000

    Add environment variable check to enable documentation build in environment without Nuke

--- nukecontexts/__init__.py
+++ nukecontexts/__init__.py
@@ -1,17 +1,21 @@
+import os
 import re
 import sys
 import platform
 
-if platform.system() == 'Darwin':
-    application = r'Nuke\d+\.\d+v\d+.app'
-elif platform.system() == 'Windows':
-    application = r'Nuke\d+\.\d+.exe'
-else:
-    raise RuntimeError('OS {0} is not supported'.format(platform.system()))
+try:
+    os.environ['DOCS_CONTEXT']
+except:
+    if platform.system() == 'Darwin':
+        application = r'Nuke\d+\.\d+v\d+.app'
+    elif platform.system() == 'Windows':
+        application = r'Nuke\d+\.\d+.exe'
+    else:
+        raise RuntimeError('OS {0} is not supported'.format(platform.system()))
 
-match = re.search(application, sys.executable)
-if not match:
-    raise RuntimeError('Import nukecontexts from within Nuke')
+    match = re.search(application, sys.executable)
+    if not match:
+        raise RuntimeError('Import nukecontexts from within Nuke')
 
 __version__ = '0.1'
 __all__ = ['ctx']
