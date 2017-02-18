def127154df4cb4d3bae12976001012330d2018a
Florian Einfalt <florian.einfalt@me.com>
2017-02-18 14:35:46 +0000

    Version v0.1.3 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ test_requirements = ['pytest', 'pytest-cov',
 
 setup(
     name = 'nukecontexts',
-    version = '0.1.2',
+    version = '0.1.3',
     description='A library of composable context managers for Nuke',
     long_description=readme,
     url='https://github.com/florianeinfalt/nukecontexts',
